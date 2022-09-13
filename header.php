6f52209f5cc05dacb15689b0d142798d22693b30
HoncoopMilan <Honcoopmilan@gmail.com>
2022-09-13 09:08:51 +0200

    Header begin
    het begin van de header het is nog niet af

---
+++ header.php
@@ -0,0 +1,13 @@
+<header>
+	<div class="containerHeader">
+		<img src="img/curio.jpg" alt="Curio">
+		<nav>
+			<a href="">Vakken</a>
+			<a href="">Rooster</a>
+			<a href="">Blokken</a>
+			<a href="">Versnellen</a>
+			<a href="">Docenten</a>
+		</nav>
+		
+	</div>
+</header>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Pra C1</title>
+	<link rel="stylesheet" href="css/main.css">
+	
+</head>
+<body>
+	<header>
+		<?php
+			require_once 'header.php';
+		?>
+	</header>
+	<main>
+		<h1>Vakken</h1>
+	</main>
+</body>
+</html>
